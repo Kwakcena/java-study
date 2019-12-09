9ab938d9ebdb5ea05001a1acacab90d2cfc387a1
KwakCena <rhkrgudwh@gmail.com>
2019-12-10 00:41:03 +0900

    묵시적 형 변환(업캐스팅)
    하위 클래스의 타입은 상위 클래스의 타입으로 묵시적인 형 변환이 가능하다.

--- Chapter7/src/inheritance/CustomerTest.java
+++ Chapter7/src/inheritance/CustomerTest.java
@@ -10,7 +10,14 @@ public class CustomerTest {
         System.out.println(customerLee.showCustomerInfo());
          */
 
-        VIPCustomer customerKim = new VIPCustomer(10020, "김유신");
+        //상위 클래스로의 묵시적 형 변환
+        /*
+        상위 클래스 형으로 변수를 선언하고 하위 클래스 인스턴스를 생성할 수 있다.
+        하위 클래스는 상위 클래스의 타입을 내포하고 있으므로 상위 클래스로 묵시적 형 변환이 가능하다.
+        상속관계에서 모든 하위 클래스는 상위클래스로 묵시적 형변환이 된다.
+        그 역은 성립하지 않는다.
+         */
+        Customer customerKim = new VIPCustomer(10020, "김유신");
         customerKim.bonusPoint = 10000;
         System.out.println(customerKim.showCustomerInfo());
     }
