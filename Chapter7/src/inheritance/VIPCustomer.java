9c98cac11d1196fe9e6ace039c97f1089534927d
KwakCena <rhkrgudwh@gmail.com>
2019-12-23 19:10:28 +0900

    오버라이딩이란?
    오버라이딩은 상위 클래스에 정의된 메서드의 구현 내용이 하위 클래스에서 구현할
    내용과 맞지 않는 경우 하위 클래스에서 동일한 이름의 메서드를 재정의 해서 사용하는 문법이다.
    
    현재 예제에서는 일반 Customer 클래스의 calcPrice 메서드와 달리 VIPCustomer의 calcPrice는
    할인율이 더 큰 폭으로 계산되어야 하므로 오벌라이딩이 필요하게 된다.

--- Chapter7/src/inheritance/VIPCustomer.java
+++ Chapter7/src/inheritance/VIPCustomer.java
@@ -1,6 +1,6 @@
 package inheritance;
 
-public class VIPCustomer extends Customer{
+public class VIPCustomer extends Customer {
 
     double salesRatio;
     private int agentID;
@@ -24,4 +24,17 @@ public class VIPCustomer extends Customer{
         salesRatio = 0.1;
         System.out.println("VIPCustomer(int, String)생성자 호출");
     }
+
+    @Override
+    public int calcPrice(int price) {
+        /*
+        오버로딩 : 메서드 이름은 같지만 매개변수의 개수, 내용이 다른 경우.
+        오버라이딩 : 상위 클래스에 정의된 메서드의 구현 내용이 하위 클래스에서 구현할 내용과 맞지 않는 경우
+                  하위 클래스에서 동일한 이름의 메서드를 재정의 할 수 있다.
+         */
+
+        bonusPoint += price * bonusRatio;
+        return price - (int)(price * salesRatio);
+    }
+
 }
