9aa1513ca369d0469afa0e9a1989735d4aacf9c1
KwakCena <rhkrgudwh@gmail.com>
2020-01-06 09:25:22 +0900

    다운 캐스팅
    이 코드에서 Animal 이라는 자료형으로 여러 인스턴스를 선언했다. 따라서
    각각의 클래스에 존재하는 readBooks, hunting, flying 메서드는 접근할 수 없다.
    여태까지는 자신의 부모 클래스인 Animal의 move에 접근해서 오버라이딩 된 move에만
    접근해서 사용할 수 있었는데, 각 클래스의 메서드를 사용하기 위해서는 다운 케스팅을
    해야 한다. 따라서 instanceof 라는 문법을 배워봤다.
    
    instanceof 문법은 자신이 어떤 인스턴스인지를 true, false로 반환해준다.

--- Chapter7/src/polymorphism/AnimalTest.java
+++ Chapter7/src/polymorphism/AnimalTest.java
@@ -1,15 +1,22 @@
 package polymorphism;
 
+import java.util.ArrayList;
+
 class Animal {
     public void move() {
         System.out.println("동물이 움직입니다.");
     }
+
 }
 
 class Human extends Animal {
     //오버라이딩.
     public void move() {
         System.out.println("사람이 두 발로 걷습니다.");
+    }
+
+    public void readBooks() {
+        System.out.println("사람이 책을 읽습니다.");
     }
 }
 
@@ -18,12 +25,19 @@ class Tiger extends Animal {
     public void move() {
         System.out.println("호랑이가 네 발로 뜁니다.");
     }
+
+    public void hunting() {
+        System.out.println("호랑이가 사냥을 합니다.");
+    }
 }
 
 class Eagle extends Animal {
     //오버라이딩.
     public void move() {
         System.out.println("독수리가 하늘을 날아갑니다.");
+    }
+    public void flying() {
+        System.out.println("독수리가 날개를 쭉 펴고 멀리 날아갑니다.");
     }
 }
 
@@ -35,30 +49,45 @@ public class AnimalTest {
         Animal tAnimal = new Tiger();
         Animal eAnimal = new Eagle();
 
+        ArrayList<Animal> animalList = new ArrayList<>();
+        animalList.add(hAnimal);
+        animalList.add(tAnimal);
+        animalList.add(eAnimal);
+
         AnimalTest test = new AnimalTest();
-        test.moveAnimal(hAnimal);
-        test.moveAnimal(tAnimal);
-        test.moveAnimal(eAnimal);
+        test.testDownCasting(animalList);
+
+
+    }
+
+    public void testDownCasting(ArrayList<Animal> list) {
+        //Animal 자료형인 hAnimal은 readBooks를 사용할 수 없다.
+        //따라서 다운 캐스팅을 해야 한다.
+        //hAnimal의 인스턴스가 Human인지를 True, false로 반환함.
+        for(int i=0; i<list.size(); i++) {
+            Animal animal = list.get(i);
+            if(animal instanceof Human) {
+                Human human = (Human)animal;
+                human.readBooks();
+            }
+            else if(animal instanceof Tiger) {
+                Tiger tiger = (Tiger)animal;
+                tiger.hunting();
+            }
+            else if(animal instanceof Eagle) {
+               Eagle eagle = (Eagle)animal;
+               eagle.flying();
+            }
+            else {
+                System.out.println("error");
+            }
+
+        }
     }
 
     //하나의 코드가 여러 자료형으로 구현되어 다른 실행이 되는 것. 다형성.
     public void moveAnimal(Animal animal) {
        animal.move();
     }
-
-    /* 원래는 아래와 같이 오버로딩 해서 자료형에 맞게 따로 구현해야 함.
-    public void moveAnimal(Human animal) {
-        animal.move();
-    }
-
-    public void moveAnimal(Tiger animal) {
-        animal.move();
-    }
-
-    public void moveAnimal(Eagle animal) {
-        animal.move();
-    }
-     */
-
 }
 
